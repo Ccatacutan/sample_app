1043146c64c289e6ecd57163ed9eabff31feba1e
Ccatacutan <catacutancarlo5@gmail.com>
2019-07-07 18:21:11 +0800

    Add account activation, only locally

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,10 +1,9 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-
   def setup
     @user = User.new(name: "Example User", email: "user@example.com",
-        password: "foobar", password_confirmation: "foobar")
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -65,5 +64,7 @@ test "email addresses should be unique" do
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
-
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?(:remember, '')
+  end
 end
