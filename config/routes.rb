a650700442bb4f71d0dd5c94721ed3862e5af291
Ccatacutan <catacutancarlo5@gmail.com>
2019-07-07 22:34:21 +0800

    Add user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
-
 Rails.application.routes.draw do
   root   'static_pages#home'
   get    '/help',    to: 'static_pages#help'
@@ -11,4 +10,5 @@ Rails.application.routes.draw do
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
 end
